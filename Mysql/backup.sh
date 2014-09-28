219fef441ba946ca8ce3083d00a4e77c9b63a2fe
yhostc <yhostc@gmail.com>
2014-09-28 18:47:01 +0800

    Delete backup.sh

--- Mysql/backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Name:mysql.sh
-# This is a ShellScript For Auto DB Backup and Delete old Backup
-#
-backupdir=/backup/mysql
-time=` date +%Y%m%d%H `
-/usr/bin/mysqldump -u wecook -p123456 wecook | gzip > $backupdir/wecook$time.sql.gz
-# only remain 7 day
-find $backupdir -name "wecook_*.sql.gz" -type f -mtime +7 -exec rm {} \; > /dev/null 2>&1
